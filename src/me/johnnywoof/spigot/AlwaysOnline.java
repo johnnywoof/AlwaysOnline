287388e5457976213dcda10933a58ff994b91e4a
Johnnywoof <c2972515@drdrb.com>
2014-11-26 20:44:41 -0500

    Minor fix

--- src/me/johnnywoof/spigot/AlwaysOnline.java
+++ src/me/johnnywoof/spigot/AlwaysOnline.java
@@ -122,7 +122,7 @@ public class AlwaysOnline extends JavaPlugin{
 			
 			this.getLogger().info("Getting HTTP cookies and random user agent for xpaw...");
 			
-			this.xm = new XpawManager(yml.getString("useragent-url"), yml.getBoolean("offline-quite-slow"));
+			this.xm = new XpawManager(yml.getBoolean("offline-quite-slow"));
 			
 			this.getLogger().info("Finished getting the data!");
 			
